5706513fe9e91182dd5869839d05e7453e7e0790
BEATRIZ AREVALO FREITAS <beatriz_a_freitas@estudante.sesisenai.org.br>
2024-10-30 20:04:09 -0300

    feat: add banco de dados

--- dataBase/db.js
+++ dataBase/db.js
@@ -1,1 +1,13 @@
-//db
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize('postgresql://andre_rosa1:rgHPNt2eJEL1nc-Tp1-6Vw@sulky-chicken-2756.jxf.gcp-southamerica-east1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full')
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+
+
+export default sequelize
